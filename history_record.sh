b67e42783d2b7ce28c3d8080729671983ba3351c
BenjaminChangSummer <=>
2015-02-06 00:36:40 +0800

    User can choose how many commands he want to see.

--- history_record.sh
+++ history_record.sh
@@ -1,6 +1,107 @@
 #!/bin/bash
+choose=0
+echo -e "Welcome!\n"
+echo -n "How many commonly-used commands would you want to see(less than 5)?"
+read choose
+    if [ "$choose" == 1 ]; then
+        echo -n "The most commonly used command is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $2}'
 
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        rm count.txt
+    elif [ "$choose" == 2 ]; then
+        echo -n "The most one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $2}'
 
-echo -e "Welcome! \nPlease choose a work to do:\n"
-echo -n "The most commonly used command is: "
-cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The second one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==2 {print $2}'
+
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==2 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        rm count.txt
+    elif [ "$choose" == 3 ]; then
+        echo -n "The most one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $2}'
+
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The second one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==2 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==2 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The third one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==3 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==3 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        rm count.txt
+    elif [ "$choose" == 4 ]; then
+        echo -n "The most one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $2}'
+
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The second one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==2 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==2 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The third one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==3 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==3 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The forth one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==4 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==4 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        rm count.txt
+    elif [ "$choose" == 5 ]; then
+        echo -n "The most one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $2}'
+
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The second one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==2 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==2 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The third one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==3 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==3 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The forth one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==4 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==4 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        #
+        echo -n "The fifth one is: "
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==5 {print $2}'
+        cat ~/.bash_history | awk -F " " '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==5 {print $1}' > count.txt
+        echo -e "\b times." >> count.txt
+        echo -n "It has been used for " ; cat count.txt
+        rm count.txt
+    fi
